60f1f9dcded6ffaab998832cb4f3c69b3d127ee4
Harvey Sanders <sanders.harvey@gmail.com>
2023-12-01 13:36:44 -0500

    Refactor for part2; part1 pass; part2 fail

--- day01-trebuchet/trebuchet.go
+++ day01-trebuchet/trebuchet.go
@@ -6,28 +6,46 @@ import (
 	"io"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func ParseCalibrationDoc(input io.Reader) (int, error) {
+type Treb struct {
+	numRE *regexp.Regexp
+}
+
+func New(numberRegExp string) *Treb {
+	return &Treb{
+		numRE: regexp.MustCompile(numberRegExp),
+	}
+}
+
+func (t Treb) ParseCalibrationDoc(input io.Reader) (int, error) {
 	scr := bufio.NewScanner(input)
 
-	re := regexp.MustCompile(`\d{1}`)
 	var nums []int
 
 	for scr.Scan() {
 		line := scr.Bytes()
-		matches := re.FindAll(line, -1)
-		var digits []byte
+		matches := t.numRE.FindAll(line, -1)
+		var digits strings.Builder
+		var firstDigit string
+		var lastDigit string
+
 		switch len(matches) {
 		case 0:
 			return 0, fmt.Errorf("no digits found in %q", string(line))
 		case 1:
-			digits = append(matches[0], matches[0]...)
+			firstDigit = wordToN(string(matches[0]))
+			lastDigit = firstDigit
 		default:
-			digits = append(matches[0], matches[len(matches)-1]...)
+			firstDigit = wordToN(string(matches[0]))
+			lastDigit = wordToN(string(matches[len(matches)-1]))
 		}
 
-		n, err := strconv.Atoi(string(digits))
+		digits.WriteString(firstDigit)
+		digits.WriteString(lastDigit)
+
+		n, err := strconv.Atoi(digits.String())
 		if err != nil {
 			return 0, fmt.Errorf("strconv.Atoi: %w", err)
 		}
@@ -40,4 +58,29 @@ func ParseCalibrationDoc(input io.Reader) (int, error) {
 	}
 
 	return sum, nil
+}
+
+// WordToN takes a spelled out number and converts it to the arabic numeral representation. If the input can not be transformed to the arabic represent, the value is returned unchanged.
+// Example:
+// wordToN("three") -> "3"
+// wordToN("3") -> "3"
+// wordToN("taco") -> "taco"
+func wordToN(word string) string {
+	nums := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	n, ok := nums[string(word)]
+	if !ok {
+		return string(word)
+	}
+	return n
 }
--- day01-trebuchet/trebuchet_test.go
+++ day01-trebuchet/trebuchet_test.go
@@ -10,18 +10,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type comparator int
+
+const (
+	EQUAL comparator = iota
+	GREATER_THAN
+	LESS_THAN
+)
+
 //go:embed input/*
 var inputFiles embed.FS
 
-func TestParseCalibrationDoc(t *testing.T) {
+func TestParseCalibrationDocSample(t *testing.T) {
 	t.Run("it parses the numbers from each line and sums the results", func(t *testing.T) {
 		testCases := []struct {
 			description string
+			numberRE    string
 			input       io.Reader
 			wantSum     int
 		}{
 			{
-				description: "sample input",
+				description: "part 1 sample input",
+				numberRE:    `\d{1}`,
 				input: strings.NewReader(`1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
@@ -29,25 +39,73 @@ treb7uchet
 `),
 				wantSum: 142,
 			},
+			{
+				description: "part 2 sample input",
+				numberRE:    `(\d|one|two|three|four|five|six|seven|eight|nine){1}`,
+				input: strings.NewReader(`two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+`),
+				wantSum: 281,
+			},
 		}
 
 		for _, tc := range testCases {
 			t.Run(tc.description, func(t *testing.T) {
-				gotSum, err := trebuchet.ParseCalibrationDoc(tc.input)
+				parser := trebuchet.New(tc.numberRE)
+				gotSum, err := parser.ParseCalibrationDoc(tc.input)
 				require.NoError(t, err)
 
 				require.Equal(t, tc.wantSum, gotSum)
 			})
 		}
 	})
+}
 
-	t.Run("works on part1 input file", func(t *testing.T) {
-		f, err := inputFiles.Open("input/part1.txt")
-		require.NoError(t, err)
+func TestParseCalibrationDocFull(t *testing.T) {
+	testCases := []struct {
+		description string
+		numberRE    string
+		wantSum     int
+		comparator  comparator
+	}{
+		{
+			description: "part 1 full input",
+			numberRE:    `\d{1}`,
+			wantSum:     53651,
+			comparator:  EQUAL,
+		},
+		{
+			description: "part 2 full input",
+			numberRE:    `(\d|one|two|three|four|five|six|seven|eight|nine){1}`,
+			wantSum:     53896,
+			comparator:  LESS_THAN, // 53896 is too high!
+		},
+	}
 
-		gotSum, err := trebuchet.ParseCalibrationDoc(f)
-		require.NoError(t, err)
+	f, err := inputFiles.Open("input/input.txt")
+	require.NoError(t, err)
 
-		require.Equal(t, 53651, gotSum)
-	})
+	for _, tc := range testCases[1:] {
+		t.Run(tc.description, func(t *testing.T) {
+			parser := trebuchet.New(tc.numberRE)
+			gotSum, err := parser.ParseCalibrationDoc(f)
+			require.NoError(t, err)
+
+			switch tc.comparator {
+			case EQUAL:
+				require.Equal(t, tc.wantSum, gotSum)
+			case GREATER_THAN:
+				require.Greater(t, gotSum, tc.wantSum)
+			case LESS_THAN:
+				require.Less(t, gotSum, tc.wantSum)
+			default:
+				require.Equal(t, tc.wantSum, gotSum)
+			}
+		})
+	}
 }
